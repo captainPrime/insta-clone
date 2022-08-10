352cc054034391e1abc5f7393097f4534d1f4135
oyindamola <taiwooyindamola732@gmail.com>
2022-08-09 17:35:54 -0700

    get users dummy data

---
+++ src/components/home/Stories.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet} from 'react-native'
+import React from 'react'
+
+
+
+const stories = () => {
+  return (
+    <View>
+      <Text>stories</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      backgroundColor: 'black',
+      flex: 1
+  }
+})
+
+export default stories
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {View, Text, SafeAreaView, StyleSheet} from "react-native"
 import Header from '../components/home/Header'
+import Stories from '../components/home/Stories'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
         <Header />
+        <Stories/>
     </SafeAreaView>
   )
 }
